e4f0169cc083ac757dcc39595ba5d4636653ea14
lobaksetve@naver.com <lobasketve@naver.com>
2024-02-19 22:09:28 +0900

    Scroll View가 안됨...

--- AutoLayoutMission/MissionViewController.swift
+++ AutoLayoutMission/MissionViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 class MissionViewController: UIViewController {
 
     @IBOutlet weak var secondView: UIView!
-    
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        secondView.layer.cornerRadius = 20
+        secondView.layer.cornerRadius = 15
+        scrollView.layer.cornerRadius = 15
     }
 }
 
